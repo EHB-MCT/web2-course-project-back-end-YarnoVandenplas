49f25368d6ab5220c7c01375c8ce514f69d0c915
YarnoVandenplas <Yarno.Vandenplas@student.ehb.be>
2026-01-03 04:15:09 +0100

    cors added cuz wanted to annoy me

--- index.js
+++ index.js
@@ -1,50 +1,53 @@
 import "dotenv/config";
 import express from "express";
 import { MongoClient, ServerApiVersion } from "mongodb";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
+
 app.use(express.json());
+app.use(cors());
 
 const uri = process.env.mongoUrl;
 if (!uri) {
-    throw new Error("mongoUrl is not defined in environment variables");
+	throw new Error("mongoUrl is not defined in environment variables");
 }
 
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    },
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
 });
 
 async function startServer() {
-    try {
-        await client.connect();
+	try {
+		await client.connect();
 
-        // GET /questions
-        app.get("/questions", async (req, res) => {
-            try {
-                const quizDb = client.db("quiz");
-                const questionsCollection = quizDb.collection("questions");
+		//GET /questions
+		app.get("/questions", async (req, res) => {
+			try {
+				const quizDb = client.db("quiz");
+				const questionsCollection = quizDb.collection("questions");
 
-                const questions = await questionsCollection.find().toArray();
+				const questions = await questionsCollection.find().toArray();
 
-                res.send(questions);
-            } catch (err) {
-                console.error(err);
-                res.send("Server error");
-            }
-        });
+				res.send(questions);
+			} catch (err) {
+				console.error(err);
+				res.send("Server error");
+			}
+		});
 
-        app.listen(port, () => {
-            console.log(`Server running on http://localhost:${port}`);
-        });
-    } catch (err) {
-        console.error(err);
-        process.exit(1);
-    }
+		app.listen(port, () => {
+			console.log(`Server running on http://localhost:${port}`);
+		});
+	} catch (err) {
+		console.error(err);
+		process.exit(1);
+	}
 }
 
 startServer();
